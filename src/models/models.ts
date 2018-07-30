2f97101630853a7d73f73bc7c40aeb8f17fbbeac
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-07-30 19:19:29 +0700

    Add period duration to Period model using moment.js

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export enum DayOfWeek {
   Monday = 'mon',
   Tuesday = 'tue',
@@ -18,6 +20,27 @@ export class Subject {
 }
 
 export class Period {
+  private _length: string = null;
+
   constructor(public day: DayOfWeek, public startTime: string, public endTime: string, 
     public room: string, public subId: string, public id?: number) {}
+
+    get length(): string {
+      if (this._length !== null) {
+        return this._length;
+      }
+
+      let date = '1-1-1970';
+      let start = moment(`${date} ${this.startTime}`, 'DD-MM-YYYY HH:mm');
+      let end = moment(`${date} ${this.endTime}`, 'DD-MM-YYYY HH:mm');
+      let duration = moment.duration(end.diff(start));
+
+      this._length = `${duration.hours()} ชั่วโมง`;
+      
+      if (duration.minutes() !== 0) {
+        this._length += ` ${duration.minutes()} นาที`;
+      }
+
+      return this._length;
+    }
 }
