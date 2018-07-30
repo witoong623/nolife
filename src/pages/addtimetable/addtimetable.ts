6f2abb6281bbaf9682fc72e0ab6a32347ab0c319
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-07-30 11:24:27 +0700

    Implement add/get subject functions

--- src/pages/addtimetable/addtimetable.ts
+++ src/pages/addtimetable/addtimetable.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, DateTime } from 'ionic-angular';
 import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
-
-/**
- * Generated class for the AddtimetablePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Semester, Subject, Period, DayOfWeek } from '../../models/models';
+import { AppDbProvider } from '../../providers/app-db/app-db';
 
 @IonicPage()
 @Component({
@@ -16,11 +11,18 @@ import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
 })
 export class AddtimetablePage {
   form: FormGroup;
+  availableSemesters: Semester[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private appDb: AppDbProvider) {
+    const currentYear = (new Date()).getFullYear();
+    const currentSemester = this.getCurrentSemester();
+
+    // TODO: do i have to specify default value of semester
     this.form = this.formBuilder.group({
       subId: ['', Validators.required],
       subName: ['', Validators.required],
+      lecturer: ['', Validators.required],
+      semester: `${currentSemester} ${currentYear}`,
       periods: this.formBuilder.array([this.initStudyTimeField()])
     });
   }
@@ -28,8 +30,9 @@ export class AddtimetablePage {
   initStudyTimeField(): FormGroup {
     return this.formBuilder.group({
       day: ['', Validators.required],
-      startTime: ['', Validators.required],
-      endTime: ['', Validators.required]
+      startTime: ['08:00', Validators.required],
+      endTime: ['11:30', Validators.required],
+      room: ''
     });
   }
 
@@ -45,5 +48,38 @@ export class AddtimetablePage {
 
   onSubmit(val: any): void {
     console.log(val);
+    let splitedSemester: string[] = val.semester.split(' ');
+    let semester = new Semester(parseInt(splitedSemester[0]), parseInt(splitedSemester[1]));
+    let s = new Subject(val.subId, val.subName, val.lecturer, semester);
+    let periods: Period[] = [];
+
+    val.periods.forEach(period => {
+      let np = new Period(<DayOfWeek>period.day, period.startTime, period.endTime, period.room, val.subId);
+      periods.push(np);
+    });
+    s.periods = periods;
+    
+    this.appDb.saveTimetable(s)
+        .then(() => this.navCtrl.pop())
+        .catch(e => console.log(e));
+  }
+
+  ionViewDidLoad() {
+    this.appDb.getAllSemesters()
+        .then(semesters => this.availableSemesters = semesters)
+        .catch(e => console.log('AddtimetablePage', e));
+  }
+
+  // TODO: this method depends on fixed pattern of semester in Thailand
+  private getCurrentSemester(): number {
+    const currentMonth = (new Date()).getMonth();
+
+    if (currentMonth >= 9) {
+      return 2;
+    } else if (currentMonth >= 5) {
+      return 1;
+    } else {
+      return 3
+    }
   }
 }
--- src/providers/app-db/app-db.ts
+++ src/providers/app-db/app-db.ts
@@ -1,14 +1,6 @@
-import { Semester } from './../../models/semester';
+import { Semester, Period, Subject } from '../../models/models';
 import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
-
-//import { SQLiteObject } from '../../mocks/sqlite/sqlitemock';
-
-export class ValueAlreadyExistError extends Error {
-  constructor(public name: string) {
-    super()
-  }
-}
 
 @Injectable()
 export class AppDbProvider {
@@ -26,28 +18,67 @@ export class AppDbProvider {
 
   getAllSemesters(): Promise<Semester[]> {
     let sql = 'select * from semesters';
-    return new Promise((resolve, reject) => {
-      this.dbObjectPromise.then(db => {
-        db.executeSql(sql, {}).then(results => {
-          let resultsList: Semester[] = [];
-          for (let i = 0; i < results.rows.length; i++) {
-            let item = results.rows.item(i);
-            resultsList.push(new Semester(item.semester, item.year));
-          }
-          resolve(resultsList);
-        });
+    return this.dbObjectPromise
+      .then(db => db.executeSql(sql, {}))
+      .then(results => {
+        let resultsList: Semester[] = [];
+        for (let i = 0; i < results.rows.length; i++) {
+          let item = results.rows.item(i);
+          resultsList.push(new Semester(item.semester, item.year));
+        }
+        return resultsList;
       });
-    });
   }
 
-  saveSemester(semester: Semester): Promise<any> {
-    let sql = 'insert into semesters(semester, year) values(?, ?)';
-    return new Promise((resolve, reject) => {
-      this.dbObjectPromise.then(db => {
-        db.executeSql(sql, [semester.semester, semester.year])
-          .then(() => resolve(), (e) => reject(e));
+  getAllSubjects(curSemester: Semester): Promise<Subject[]> {
+    let selectSub = 'select * from subjects where semester = ? and year = ?';
+    return this.dbObjectPromise
+      .then(db => db.executeSql(selectSub, [curSemester.semester, curSemester.year]))
+      .then(results => {
+        let subjectList: Subject[] = [];
+        for (let i = 0; i < results.rows.length; i++) {
+          let row = results.rows.item(i);
+          let s = new Semester(row.semester, row.year);
+          subjectList.push(new Subject(row.subId, row.name, row.lecturer, s))
+        }
+
+        return subjectList;
       });
-    });
+  }
+
+  getPeriods(subId: string): Promise<Period[]> {
+    let sql = 'select * from periods where subId = ?';
+    return this.dbObjectPromise
+      .then(db => db.executeSql(sql, [subId]))
+      .then(results => {
+        let periodsList: Period[] = [];
+        for (let i = 0; i < results.rows.length; i++) {
+          let row = results.rows.item(i);
+          periodsList.push(new Period(row.day, row.startTime, row.endTime, row.room, subId));
+        }
+
+        return periodsList;
+      });
+  }
+
+  saveSemester(semester: Semester): Promise<void> {
+    let sql = 'insert into semesters(semester, year) values(?, ?)';
+    return this.dbObjectPromise.then(db => db.executeSql(sql, [semester.semester, semester.year]))
+  }
+
+  saveTimetable(sub: Subject): Promise<void> {
+    let insertSubject = 'insert into subjects(subId, name, lecturer, semester, year) values(?,?,?,?,?)';
+    let insertPeriods = 'insert into periods(day, startTime, endTime, room, subId) values(?,?,?,?,?)';
+    const periods = sub.periods;
+    return this.dbObjectPromise
+      .then(db => {
+        db.transaction(tx => {
+          tx.executeSql(insertSubject, [sub.subId, sub.name, sub.lecturer, sub.semester.semester, sub.semester.year])
+          periods.forEach(period => {
+            tx.executeSql(insertPeriods, [period.day, period.startTime, period.endTime, period.room, period.subId]);
+          });
+        });
+      });
   }
 
   private connectToDb(): Promise<SQLiteObject> {
@@ -55,8 +86,11 @@ export class AppDbProvider {
       this.sqlite.create(this.options)
         .then((db: SQLiteObject) => {
           let semestersSql = 'create table if not exists semesters (semester integer, year integer, primary key (semester, year))';
-          let subjectSql = 'create table if not exists subjects (subId text primary key, name text)';
-          let periodSql = 'create table if not exists periods (id integer primary key, day text, startTime text, endTime text)';
+          let subjectSql = 'create table if not exists subjects (subId text primary key, name text, ' +
+              'lecturer text, semester integer, year integer, ' + 
+              'foreign key(semester) references semesters(semester), foreign key(year) references semesters(year))';
+          let periodSql = 'create table if not exists periods (id integer primary key, day text, startTime text, endTime text, room text, ' +
+              'subId text, foreign key(subId) references subjects(subId))';
           let semesterPromise = db.executeSql(semestersSql, {});
           let subjectPromise = db.executeSql(subjectSql, {});
           let periodsPromise = db.executeSql(periodSql, {});
