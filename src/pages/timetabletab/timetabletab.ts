9206767d88ca4e35937799719cac8eecf717bd65
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-07-30 19:21:04 +0700

    Initial implement timetable tab

--- src/pages/timetabletab/timetabletab.ts
+++ src/pages/timetabletab/timetabletab.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Subject, Period } from '../../models/models';
 
 @IonicPage()
 @Component({
@@ -8,9 +9,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TimetabletabPage {
   dayOftimetable: string;
+  displayTitle: string;
+  subjects: Subject[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.dayOftimetable = navParams.data.displayTitle;
+    this.dayOftimetable = navParams.data.dayOftimetable;
+    this.displayTitle = navParams.data.displayTitle;
+    let tempSubjects: Subject[] = navParams.data.data.slice();
+
+    // filter subjects that don't have lecture today
+    tempSubjects = tempSubjects.filter(subject => {
+      for (let period of subject.periods) {
+        if (period.day === this.dayOftimetable) {
+          return true;
+        }
+      }
+      return false;
+    });
+
+    this.subjects = tempSubjects;
   }
 
 }
