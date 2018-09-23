d0ab4b5e2fcbcf12600bc6bfddf25e4e19330091
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-09-23 13:39:16 +0700

    Add subject delete functionality to subject page

--- src/pages/subject/subject.ts
+++ src/pages/subject/subject.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, AlertController } from 'ionic-angular';
 import { Subject } from '../../models/models';
 import { AppDbProvider } from '../../providers/app-db/app-db';
 import { getCurrentSemester } from '../../utilities/datetimeutility';
@@ -14,8 +14,45 @@ import { SubjectDetailPage } from '../subject-detail/subject-detail';
 export class SubjectPage {
   subjects: Subject[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private appDb: AppDbProvider) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private appDb: AppDbProvider,
+              private actionSheetCtrl: ActionSheetController,
+              private alertCtrl: AlertController) {
 
+  }
+
+  onPress(subject: Subject): void {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'ต้องการ',
+      buttons: [
+        {
+          text: 'ลบ',
+          role: 'destructive',
+          handler: () => {
+            let alert = this.alertCtrl.create({
+              title: `ลบ ${subject.name}?`,
+              message: `ต้องการจะลบ ${subject.name} ใช่หรือไม่`,
+              buttons: [
+                {
+                  text: 'ยกเลิก',
+                  role: 'cancel'
+                },
+                {
+                  text: 'ตกลง',
+                  handler: () => {
+                    this.appDb.deleteSubject(subject.subId, subject.semester).then(() => this.loadSubjects());
+                  }
+                }
+              ]
+            });
+            alert.present();
+          },
+          icon: 'trash'
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
   subjectSelected(subject: Subject): void {
