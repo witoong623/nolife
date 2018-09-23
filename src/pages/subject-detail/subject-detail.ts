3214b326fe499a06ef72c860306b07818ff70883
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2018-09-23 15:49:21 +0700

    Add delete period to subject detail page

--- src/pages/subject-detail/subject-detail.ts
+++ src/pages/subject-detail/subject-detail.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Subject } from '../../models/models';
+import { IonicPage, NavController, NavParams, ActionSheetController, AlertController } from 'ionic-angular';
+import { Subject, Period } from '../../models/models';
 import { AppDbProvider } from '../../providers/app-db/app-db';
 import { getCurrentSemester } from '../../utilities/datetimeutility';
 
@@ -12,13 +12,53 @@ import { getCurrentSemester } from '../../utilities/datetimeutility';
 export class SubjectDetailPage {
   public subject: Subject
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private appDb: AppDbProvider) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private appDb: AppDbProvider,
+              private actionSheetCtrl: ActionSheetController,
+              private alertCtrl: AlertController) {
     this.subject = this.navParams.get('subject');
   }
 
   ionViewDidLoad() {
     this.appDb.getPeriods(this.subject.subId, getCurrentSemester())
       .then(periods => this.subject.periods = periods);
+  }
+
+  onPress(period: Period): void {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'ต้องการ',
+      buttons: [
+        {
+          text: 'ลบ',
+          role: 'destructive',
+          handler: () => {
+            let alert = this.alertCtrl.create({
+              title: `ลบ ${period.dayOfWeekThai}?`,
+              message: `ต้องการจะลบ ${period.dayOfWeekThai} เวลา ${period.startTime} ใช่หรือไม่`,
+              buttons: [
+                {
+                  text: 'ยกเลิก',
+                  role: 'cancel'
+                },
+                {
+                  text: 'ตกลง',
+                  handler: () => {
+                    this.appDb.deletePeriod(period.id).then(() => {
+                      let index = this.subject.periods.indexOf(period);
+                      this.subject.periods.splice(index, 1);
+                    });
+                  }
+                }
+              ]
+            });
+            alert.present();
+          },
+          icon: 'trash'
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
